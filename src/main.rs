3c66ff611f7b42b56c6749c20f424c1ca80b7680
park jongheum <whdgmawkd@naver.com>
2023-05-01 18:09:32 +0900

    서버 구현

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,21 @@ use tokio::net::{TcpListener, TcpStream};
 use tokio::io::{AsyncWriteExt, AsyncReadExt};
 use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
+use tokio::sync::Mutex;
+use std::sync::Arc;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Copy, Debug)]
 struct TetrisData {
-    map: [[i32; 10]; 20], // 10x20 map
-    score: i32, // current score
+    map: [[usize; 10]; 20], // 10x20 map
+    score: usize, // current score
 }
 
 #[tokio::main]
 async fn main() {
     let mut tetris_map = HashMap::<String, TetrisData>::new();
+    let shared_map = Arc::new(Mutex::new(tetris_map));
 
-    let addr = "0.0.0.0:8888";
+    let addr = "0.0.0.0:8080";
     let listener = TcpListener::bind(addr).await.unwrap();
     println!("Server Listening on: {addr}");
     
@@ -22,36 +25,42 @@ async fn main() {
         let (mut socket, addr) = listener.accept().await.unwrap();
         let addr_string = addr.to_string();
         
-        
-        let mut buf = [0; 4096];
-        
-        let n = match socket.read(&mut buf).await {
-            Ok(n) if n == 0 => { return; },
-            Ok(n) => n,
-            Err(e) => {
-                eprintln!("failed to read from socket: {e}");
-                return;
+        let shared_map = shared_map.clone();
+        tokio::spawn(async move {
+
+            let mut map = shared_map.lock().await;
+            let mut buf = [0; 4096];
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(n) if n == 0 => { return; },
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket: {e}");
+                        return;
+                    }
+                };
+                let serialized_data = &buf[..n];
+                let data: TetrisData = serde_json::from_slice(serialized_data).unwrap();
+                println!("{}: {:?}",addr_string, data);
+                map.insert(addr_string.clone(), data.clone());
+
+                let empty_data = TetrisData {
+                    map: [[0; 10]; 20],
+                    score: 0
+                };
+
+                let mut serialized_data = serde_json::to_string(&empty_data).unwrap();
+                for (key, value) in map.clone().into_iter() {
+                    if *key != addr_string {
+                        serialized_data = serde_json::to_string(&value).unwrap();
+                        break;
+                    }
+                }
+                socket.write_all(serialized_data.as_bytes()).await.unwrap();
+                println!("{}: {}",addr_string, data.score);
             }
-        }
-        let serialized_data = &buf[..n];
-        let data = serde_json::from_slice(serialized_data).unwrap();
-        tetris_map.insert(addr_string, data);
+            // socket.shutdown().await.unwrap();
 
-        let empty_data = TetrisData {
-            map: [[0; 10]; 20],
-            score: 0
-        };
-
-        let mut serialized_data = serde_json::to_string(&empty_data).unwrap();
-        for (key, value) in &tetris_map {
-            if *key != addr_string {
-                serialized_data = serde_json::to_string(value).unwrap();
-                break;
-            }
-        }
-        socket.write_all(serialized_data.as_bytes()).await.unwrap();
-        socket.shutdown().await.unwrap();
-
-        println!("{}: {}",addr_string, data.score);
+        });
     }
 }
